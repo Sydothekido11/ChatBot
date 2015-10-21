2d5ea968a2100191ff53631d4044cf32cf7024f9
Sydothekido11 <sydnemelka@gmail.com>
2015-10-21 13:52:52 -0600

    Created the view.

--- src/chat/controller/ChatRunner.java
+++ src/chat/controller/ChatRunner.java
@@ -1,6 +1,15 @@
 package chat.controller;
 
+/**
+ * Starts the Chatbot program. 
+ *@author snem8901
+ *@version 1.0 10/21/15
+ */
 public class ChatRunner
 {
-
+	public static void main (String [] args)
+	{
+		ChatController myChatController = new ChatController();
+		myChatController.start();
+	}
 }
--- src/chat/view/ChatbotView.java
+++ src/chat/view/ChatbotView.java
@@ -1,6 +1,37 @@
 package chat.view;
 
+import javax.swing.JOptionPane;
+
+/**
+ * This class displays the popup.
+ * @author snem8901
+ *@version 1.0 10/21/15
+ */
 public class ChatbotView
 {
-
+	/**
+	 * This displays the text in a popup window and collects the user input.
+	 * @param displayText
+	 * @return
+	 */
+		public String getUserInput(String displayText)
+		{
+				
+			String answer = "";
+					
+			answer = JOptionPane.showInputDialog(null, displayText);
+			
+			return answer;
+			
+		}
+		
+		/**
+		 * This returns and displays the user's input in a separate popup window after it has been entered.
+		 * @param displayText
+		 */
+		public void displayResponse(String displayText)
+		{
+			JOptionPane.showMessageDialog(null, displayText);
+		}
 }
+
