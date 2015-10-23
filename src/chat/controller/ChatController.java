093a9fdfcee0c7065ffabbf14b26a7f0a8a74f3e
Sydothekido11 <sydnemelka@gmail.com>
2015-10-23 13:13:41 -0600

    Comments.

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -4,9 +4,9 @@ import chat.model.ChatBot;
 import chat.view.ChatbotView;
 
 /**
- * Controller for the Chatbot project. Created a popup for the user name, and displays the response.
+ * Controller for the Chatbot project. Created a popup for the user name, and displays the response. 
  * @author snem8901
- * @version 1.2 10/23/15 Displays the Chatbot's userName variable.
+ * @version 1.3 10/23/15 Displays the Chatbot's userName variable. Made a while loop as well.
  */
 public class ChatController
 {
