3fb62df6cd3a7c53dee16fbd788ecbee8c994b7b
Sydothekido11 <sydnemelka@gmail.com>
2015-12-04 12:06:00 -0700

    Updated controller documentation.

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -7,7 +7,8 @@ import chat.view.ChatFrame;
 /**
  * Controller for the Chatbot project. Created a popup for the user name, and displays the response. 
  * @author snem8901
- * @version 1.3 10/23/15 Displays the Chatbot's userName variable. Made a while loop as well.
+ * @version 1.4 12/4/15 Displays the Chatbot's userName variable. Made a while loop as well. 
+ * Added the getters for Chabot, Chatview and Chatframe
  */
 public class ChatController
 {
