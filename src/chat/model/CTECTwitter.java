856ec75f7c7a990d0202c54bca12454ba74df6e1
Sydothekido11 <snem8901@740s-pl-109.local>
2016-03-21 12:18:26 -0600

    Added a sampleInvestigation.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -178,5 +178,29 @@ public class CTECTwitter
 	}
 	
 	
+	public String sampleInvestigation()
+	{
+		String results = "";
+		
+		Query query = new Query("Your Lie in April");
+		query.setCount(100);
+		query.setGeoCode(new GeoLocation(40.587, -111.869), 5, Query.MILES);
+		query.setSince("2016-1-1");
+		try
+		{
+			QueryResult result = chatbotTwitter.search(query);
+			results.concat("Count : " + result.getTweets());
+			for (Status tweet : result.getTweets())
+			{
+				results.concat("@" + tweet.getUser().getName() + ": " + tweet.getText() + "\n");
+			}
+		}
+		catch (TwitterException error)
+		{
+			error.printStackTrace();
+		}
+		
+		return results;
+	}
 	
 }
